de061f3517c6b7952d43f50d2f308fbee52fa122
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-11-26 20:57:18 +0200

    onSubmit form

--- redux-tutorial/src/components/PostForm.js
+++ redux-tutorial/src/components/PostForm.js
@@ -9,17 +9,38 @@ class PostForm extends Component {
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
 
+    onSubmit(e) {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            text: this.state.text
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
+        
+    }
+
     render() {
         return (
             <div>
                 <h1> Add a new Post </h1>
-                <form> 
+                <form onSubmit={this.onSubmit}> 
                     <div>
                         <label> Title: </label>
                         <input type="text" name="title" 
